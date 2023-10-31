07b2db4e7830148fc54d4252ee64c3af3f2ca712
stefan-ffr <75080111+stefan-ffr@users.noreply.github.com>
2023-10-31 17:20:02 +0100

    Update install-lamp.sh

--- LAMP/install-lamp.sh
+++ LAMP/install-lamp.sh
@@ -1,36 +1,32 @@
 #!/bin/bash
-MYSQL-FILE=mysql-apt-config_0.8.22-1_all.deb
 
-#################################################################################################################################################################
+# Prompt for MySQL root password
+read -s -p "Enter MySQL root password: " mysql_root_password
+echo
 
-#Update/Upgrade Server
-apt update
-apt upgrade
-
-#Install sudo, wget, ufw and apache2
-apt install sudo -y
-sudo apt install wget -y
-sudo apt install apache2 -y
-sudo apt install ufw -y
-
-#Define Ports for LAMP
-sudo ufw app list
-sudo ufw allow OpenSSH
-sudo ufw allow 'Apache Full'
-sudo ufw status
-sudo systemctl status apache2
-
-#Install MySQL from File
-wget https://dev.mysql.com/get/$MYSQL-FILE
-sudo apt install ./$MYSQL-FILE
-
-#Install MySQL-Server
+# Update the package list
 sudo apt update
-sudo apt install mysql-server
-sudo service mysql status
-sudo mysql_secure_installation
-sudo apt -y install lsb-release apt-transport-https ca-certificates
-sudo wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
-echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/php.list
-sudo apt update
-sudo apt install -y php libapache2-mod-php php8.1-mysql php8.1-common php8.1-mysql php8.1-xml php8.1-xmlrpc php8.1-curl php8.1-gd php8.1-imagick php8.1-cli php8.1-dev php8.1-imap php8.1-mbstring php8.1-opcache php8.1-soap php8.1-zip php8.1-intl -y
+
+# Install Apache
+sudo apt install -y apache2
+
+# Install MySQL and set the root password
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $mysql_root_password"
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $mysql_root_password"
+sudo apt install -y mysql-server
+
+# Install PHP and required modules
+sudo apt install -y php libapache2-mod-php php-mysql
+
+# Enable the Apache and PHP modules
+sudo a2enmod php
+sudo systemctl restart apache2
+
+# Open the firewall for Apache
+#sudo ufw allow in "Apache Full"
+
+# Test the installation
+echo "LAMP stack is installed and running. You can access your web server at http://localhost/"
+
+# Clean up
+rm -- "$0"
